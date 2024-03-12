6d27d5c71fbeab93dc00580c5eeba4174a9ce840
xiangzhi.kong <kongxz45@163.com>
2024-03-12 14:07:43 +0800

    init project

---
+++ src/main/java/com/sbux/springbootv3demo/SpringBootV3DemoApplication.java
@@ -0,0 +1,13 @@
+package com.sbux.springbootv3demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootV3DemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootV3DemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sbux/springbootv3demo/SpringBootV3DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbux.springbootv3demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootV3DemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
