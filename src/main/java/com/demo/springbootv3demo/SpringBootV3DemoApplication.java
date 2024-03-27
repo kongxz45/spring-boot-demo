63498f564aad565c31404a7ffc8afba72449d70e
xiangzhi.kong <kongxz45@163.com>
2024-03-27 16:22:57 +0800

    random commit

--- src/main/java/com/demo/springbootv3demo/SpringBootV3DemoApplication.java
+++ src/main/java/com/demo/springbootv3demo/SpringBootV3DemoApplication.java
@@ -3,6 +3,10 @@ package com.demo.springbootv3demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * this is Application class
+ *
+ */
 @SpringBootApplication
 public class SpringBootV3DemoApplication {
 
